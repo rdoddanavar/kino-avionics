019c818eaa9ae2fd493f970dc87e297e5e904d94
Roshan Doddanavar <roshandoddanavar@gmail.com>
2019-05-14 01:43:16 -0500

    Index tool update

--- update_index.sh
+++ update_index.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 # update_index.sh
+# Takes one positional argument - tree depth level (int)
+# ex. (2 levels) ./update_index.sh 2
 
+target="/home/${USER}/Git/hpr-hardware/"
 file="/home/${USER}/Git/hpr-hardware/README.md"
 blk='```'
-lvl=3 # No. lvl in tree
+lvl=$1 # No. lvl in tree
 
 # Delete old index 
 sed "1,/$blk/!d" $file > temp
 mv temp $file
 
 # Generate new index, append to file
+cd $target
 tree -L $lvl --dirsfirst >> $file
 echo "$blk" >> $file
