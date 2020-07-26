f9b2cca8f8f0f35dad770f28477e88f486b3fb22
Roshan Doddanavar <roshandoddanavar@gmail.com>
2020-07-26 16:42:22 -0700

    exec script

---
+++ software/kino_rpi/exec.c
@@ -0,0 +1,34 @@
+/*-----------------------------------------------------------------//
+High Power Rocketry - Avionics Hardware & Software
+MIT License
+Copyright (c) 2020 Roshan Doddanavar
+https://rdoddanavar.github.io
+ _   _                       _          _       
+| |_(_)_ _  ___ ___ __ ___ _(_)___ _ _ (_)__ ___
+| / / | ' \/ _ \___/ _` \ V / / _ \ ' \| / _(_-<
+|_\_\_|_||_\___/   \__,_|\_/|_\___/_||_|_\__/__/
+
+Path:
+    kino-avionics/software/kino_rpi/main.c
+Created:
+    2020-07-26
+Type:
+    C source code
+Description:
+    Kino Raspberry Pi flight software - exec routine
+Dependencies:
+    pigpio library
+//-----------------------------------------------------------------*/
+
+// Builtin libraries
+#include <stdio.h>
+
+// Project libraries
+#include "pigpio.h"
+
+int main(void)
+{
+    printf("Hello world\n");
+
+    return 0;
+}
