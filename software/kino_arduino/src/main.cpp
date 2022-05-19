f95e4e9928ddfd06d1ff95a7fce410e87a4a4da7
rdoddanavar <roshandoddanavar@gmail.com>
2022-05-18 20:08:02 -0700

    Working SPI with Arudino Pro Mini

--- software/kino_arduino/src/main.cpp
+++ software/kino_arduino/src/main.cpp
@@ -1,16 +1,26 @@
 #include <Arduino.h>
+#include <SPI.h>
+
+//volatile byte receivedData;
+
+ISR (SPI_STC_vect)        //Inerrrput routine function 
+{
+    SPDR = 5;
+}
 
 void setup()
 {
+    Serial.begin(9600);
 
-    // init Serial USB
-    Serial.begin(115200);//for debugging  
-
+    pinMode(MISO,OUTPUT);   //Sets MISO as OUTPUT
+    SPCR |= _BV(SPE);       //Turn on SPI in Slave Mode
+    SPI.attachInterrupt();  //Activate SPI Interuupt 
 }
 
 void loop()
 { 
-    dataOut = 5;
-    Serial.println(dataOut);
+
+    Serial.println(5, HEX);
     delay(500);
+
 }
