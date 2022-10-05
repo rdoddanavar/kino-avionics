7a7d52777a317d972a85f4fca369437b80a15843
rdoddanavar <roshandoddanavar@gmail.com>
2022-10-04 23:16:21 -0700

    Attempting to use unity testing framework

--- software/kino_arduino/test/test_atmos/test_atmos_bme280.cpp
+++ software/kino_arduino/test/test_atmos/test_atmos_bme280.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <unity.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
@@ -6,22 +7,36 @@
 //----------------------------------------------------------------------------//
 
 // General setup
-const uint16_t monitorBaud = 9600; 
-const uint16_t sampleTime  = 1000; // Loop delay [ms]
+const uint32_t monitorBaud = 9600;
+const float    runTime     = 60.0f;  // Total loop time [s]
+const float    sampleRate  = 1.0f;   // Loop delay [Hz]
+const float    s_to_ms     = 1.0e3f;
 
+uint16_t nSample;
+unit16_t nDelay;
 String dataOut;
 
 // BME280 setup
 const uint8_t i2cAddress           = 0x76;
 const float   seaLevelPressure_hPa = 1013.25f;
-const float   Pa_to_hPa            = 1.0f/100.0f;
+const float   Pa_to_psi            = 0.0001450380f;
+const float   m_to_ft              = 1.0f/0.3048f;
 
 Adafruit_BME280 bme;
 
 //----------------------------------------------------------------------------//
 
+float degC_to_degF(float degC)
+{
+    return (degC*(9.0f/5.0f) + 32.0f);
+}
+
+//----------------------------------------------------------------------------//
+
 void setup()
 {
+
+    UNITY_BEGIN();
 
     Serial.begin(monitorBaud);
 
@@ -31,6 +46,8 @@ void setup()
         while (1);
     }
 
+    nSample = (uint16_t) runTime*sampleRate;
+    nDelay  = (uint16_t) (1.0f/sampleRate)*s_to_ms;
     dataOut = "";
 
 }
@@ -40,14 +57,24 @@ void setup()
 void loop()
 {
 
-    dataOut += "Temp: "  + String(bme.readTemperature())                  + "*C"  + ", ";
-    dataOut += "Press: " + String(bme.readPressure() * Pa_to_hPa)         + "hPa" + ", ";
-    dataOut += "Alt: "   + String(bme.readAltitude(seaLevelPressure_hPa)) + "deg" + ", ";
-    dataOut += "Hum: "   + String(bme.readHumidity())                     + "%"   + ""  ;
+    if (i < nSample)
+    {   
+        
+        dataOut += "Temp: "  + String(degC_to_degF(bme.readTemperature()))            + "F"   + ", ";
+        dataOut += "Press: " + String(bme.readPressure() * Pa_to_psi)                 + "psi" + ", ";
+        dataOut += "Alt: "   + String(bme.readAltitude(seaLevelPressure_hPa)*m_to_ft) + "ft"  + ", ";
+        dataOut += "Hum: "   + String(bme.readHumidity())                             + "%"   + ""  ;
 
-    Serial.println(dataOut);
-    dataOut = "";
+        Serial.println(dataOut);
+        dataOut = "";
 
-    delay(sampleTime);
+        delay(nDelay);
+        i++
+
+    }
+    else
+    {
+        UNITY_END();
+    }
 
 }
